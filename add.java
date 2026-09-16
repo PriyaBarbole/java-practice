a3f15447709dfda4d79bf0ef88db7ceef5f238be
Priya Barbole <barbolepriya@gmail.com>
2026-09-16 18:42:32 +0530

    Added Addition program

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add {
+    public static void main(String [] args){
+
+        int a = 50;
+        int b = 80;
+        int c = a + b;
+
+        System.out.println(c);
+        
+    }
+    
+}
