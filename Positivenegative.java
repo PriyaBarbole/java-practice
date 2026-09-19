dfe535907537b730329f4efb2b4c8af694e742f2
Priya Barbole <barbolepriya@gmail.com>
2026-09-19 15:55:05 +0530

    Added if-else program

---
+++ Positivenegative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Positivenegative {
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+         System.out.println("Enter your number:");
+         int a = sc.nextInt();
+
+         if(a >= 0){
+           System.out.println("Number is positive.");
+           }
+
+        else{
+           System.out.println("Number is negative.");
+           }
+    }
+    
+}
