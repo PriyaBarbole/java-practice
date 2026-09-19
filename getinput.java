1225512f5e3a99767c9950fbb1e5754dd98141fc
Priya Barbole <barbolepriya@gmail.com>
2026-09-19 15:08:42 +0530

    Added Input program

---
+++ getinput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class getinput {
+   public static void main(String [] args){
+
+   Scanner sc = new Scanner(System.in);
+   
+   System.out.println("Enter your name: ");
+   String name = sc.nextLine();
+  
+  System.out.println("Enter your age: ");
+   int age = sc.nextInt();
+
+   System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old.");
+
+   sc.close();
+    
+}
+
+}
