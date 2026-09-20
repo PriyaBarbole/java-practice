2724f60ed77e45fc73e39b6b8093be0e7642a836
Priya Barbole <barbolepriya@gmail.com>
2026-09-20 12:21:59 +0530

    Added Largest of two program

---
+++ LargestOfTwo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LargestOfTwo {
+    public static void main(String [] args){
+ 
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter 1st number: ");
+int a = sc.nextInt();
+
+System.out.print("Enter 2nd number: ");
+int b = sc.nextInt();
+
+if(a > b){
+    System.out.print("A Is greater");
+}
+else if(b > a){
+    System.out.print("B Is greater");
+}
+else{
+    System.out.print("Both are equal");
+}
+
+    }
+    
+}
